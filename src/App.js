09738bac69d49aca796791bc5a16d644ae657671
Tonymans33 <tonymansour66@yahoo.com>
2021-09-08 20:54:32 +0200

    Setting up main components & project structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import './css/App.css';
+import Sidebar from "./components/Sidebar";
+import Chat from "./components/Chat";
 
 function App() {
   return (
     <div className="App">
       <h1>Lets a build a whatsapp clone</h1>
+
+    <Sidebar />
+    <Chat />
+
     </div>
   );
 }
