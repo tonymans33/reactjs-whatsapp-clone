b1dd79559723d420a877c55cb47a2a3d2442c004
Tonymans33 <tonymansour66@yahoo.com>
2021-09-08 20:54:10 +0200

    Setting up firebase dependencies

---
+++ src/Firebase/Firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDX4kanqUGg_QpjTMZLuI3FVHXrdbMpGEU",
+    authDomain: "whatsapp-clone-daec8.firebaseapp.com",
+    projectId: "whatsapp-clone-daec8",
+    storageBucket: "whatsapp-clone-daec8.appspot.com",
+    messagingSenderId: "733421305000",
+    appId: "1:733421305000:web:366c40724c4554f25f969d",
+    measurementId: "G-1PXRX88XSD"
+};
